40d0729cf57f695d9a28d9a59989b99c4b013a4b
nindita.harahap <nindita.harahap@gmail.com>
2018-07-22 00:16:18 -0400

    Python 104 - WWCKL - Update

--- dealership/contracts.py
+++ dealership/contracts.py
@@ -1,12 +1,34 @@
 class Contract(object):
-    pass
-
+    def __init__(self, vehicle, customer):
+        self.vehicle = vehicle
+        self.customer = customer
+        
+    def total_value(self):
+        if self.customer.is_employee() == True:
+          return self.original_price() * 0.9
+        else:
+          return self.original_price()
+        
+    def monthly_value(self):
+        return self.total_value() / self.monthly_count
 
 class BuyContract(Contract):
     def __init__(self, vehicle, customer, monthly_payments):
-        pass
-
-
+        super(BuyContract, self).__init__(vehicle, customer)
+        self.monthly_payments = monthly_payments
+        self.monthly_count = monthly_payments
+    
+    def original_price(self):
+        return self.vehicle.sale_price() + (self.vehicle.INTEREST_PRICE_MULTIPLIER * self.monthly_payments * self.vehicle.sale_price() / 100) 
+                
 class LeaseContract(Contract):
     def __init__(self, vehicle, customer, length_in_months):
-        pass
+        super(LeaseContract, self).__init__(vehicle, customer)
+        self.length_in_months = length_in_months
+        self.monthly_count = length_in_months
+        
+    def lease_multiplier(self):
+        return self.vehicle.sale_price() * (self.vehicle.LEASE_PRICE_MULTIPLIER / self.length_in_months)
+      
+    def original_price(self):
+        return self.vehicle.sale_price() + self.lease_multiplier()
